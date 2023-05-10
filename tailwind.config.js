6c21bfdab89cd8dd3938415c91a19fdff8ee8ea4
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2023-05-10 22:51:58 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
       colors: {
         'main-blue': '#2400FF',
         'background': '#FEFEFE',
+        'orange': '#FFA500',
+        'teritaer': '#7B68EE',
+        'vier': '#FF7F50',
+        'fuenf': '#00BFFF'
       },
     },
   },
