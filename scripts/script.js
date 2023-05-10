d8892b79fce88556a1c66315cb1906b5eb60fdf4
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2023-05-10 17:25:58 +0200

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+      extend: {
+        colors: {
+          'main-blue': '#689048',
+          'background': '#FEFEFE',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      'FiraSans': ['"Fira Sans"', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'main-blue': '#2400FF',
+        'background': '#FEFEFE',
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+
